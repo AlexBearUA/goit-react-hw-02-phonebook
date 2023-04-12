eaecce53abfd2b2653a45e1b690992f226493ca2
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-04-12 15:27:24 +0200

    finish!!!!!

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
-// import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import css from './ContactList.module.css';
 
 export const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <ul className={css.ContactList}>
       {contacts.map(({ name, number, id }) => (
-        <li className={css.ContactListItem} key={id}>
-          <p>
-            {name}: {number}
-          </p>
-          <button onClick={() => onDeleteContact(id)}>Delete</button>
-        </li>
+        <ContactListItem
+          key={id}
+          name={name}
+          number={number}
+          id={id}
+          deleteContact={onDeleteContact}
+        />
       ))}
     </ul>
   );
