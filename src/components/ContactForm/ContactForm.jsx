a69cb53a070839c083da61aa482fc94c2bb6e6e4
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-04-12 09:38:33 +0200

    +1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -15,15 +15,17 @@ class ContactForm extends Component {
   nameInputId = nanoid();
   numberInputId = nanoid();
 
-  handleInputChange = e => {
-    const { name, value } = e.currentTarget;
+  // handleInputsChange = e => {
+  //   const { name, value } = e.currentTarget;
 
-    this.setState({ [name]: value });
-  };
+  //   this.setState({ [name]: value });
+  // };
 
   handleSubmit = e => {
     e.preventDefault();
-
+    console.log(e.target.elements.name.value);
+    this.setState({ name: e.target.elements.name.value });
+    console.log(this.state);
     this.props.onSubmit(this.state);
     this.reset();
   };
@@ -38,22 +40,22 @@ class ContactForm extends Component {
         <label htmlFor={this.nameInputId}>Name</label>
 
         <input
-          onChange={this.handleInputChange}
+          // onChange={this.handleInputsChange}
           id={this.nameInputId}
           type="text"
           name="name"
-          value={this.state.name}
+          // value={this.state.name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
         <label htmlFor={this.numberInputId}>Number</label>
         <input
-          onChange={this.handleInputChange}
+          // onChange={this.handleInputsChange}
           id={this.numberInputId}
           type="tel"
           name="number"
-          value={this.state.number}
+          // value={this.state.number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
